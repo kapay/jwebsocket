3449155b15f63bcdc3c6ab012c4164c9582a5491
mailtopuran <mailtopuran@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-03-19 04:49:32 +0000

    minor change

--- jWebSocketNettyEngine/src/main/java/org/jwebsocket/netty/connectors/NettyConnector.java
+++ jWebSocketNettyEngine/src/main/java/org/jwebsocket/netty/connectors/NettyConnector.java
@@ -91,7 +91,6 @@ public class NettyConnector extends BaseConnector {
 	public int getRemotePort() {
 		InetSocketAddress address = (InetSocketAddress) handler
 				.getChannelHandlerContext().getChannel().getRemoteAddress();
-		System.out.println("CONTEXT CHANNEL REMOTE PORT:"+address.getPort());
 		return address.getPort();
 	}
 
