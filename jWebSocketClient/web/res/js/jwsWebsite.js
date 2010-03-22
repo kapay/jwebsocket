a10f31e678c1a9a25381904cb5eefa1bd51604b3
fivefeetfurther <fivefeetfurther@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-03-22 10:32:45 +0000

    Added new jwsWebsite.js to client web (convenience tools for web site)

---
+++ jWebSocketClient/web/res/js/jwsWebsite.js
@@ -0,0 +1,20 @@
+
+
+function makeImgExp( aId ) {
+	var lImg = document.getElementById( aId );
+	if( lImg ) {
+		var lP = document.createElement( "p" );
+		lP.innerHTML = "Screenshot";
+		lP.className = "screenshotPlus";
+		lP.onclick = function() {
+			if( lImg.className != "screenshotOff" ) {
+				lImg.className = "screenshotOff" ;
+				lP.className = "screenshotPlus";
+			} else {
+				lImg.className = "screenshotOn";
+				lP.className = "screenshotMinus";
+			}
+		};
+		lImg.parentElement.insertBefore( lP, lImg );
+	}
+}
