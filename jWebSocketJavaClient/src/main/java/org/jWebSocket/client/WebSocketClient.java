87e32955120389c89a623a8bf59e5d39787d395f
fivefeetfurther <fivefeetfurther@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-05-06 11:52:28 +0000

    Splitted App Server project into embedded and stand-alone project

--- jWebSocketJavaClient/src/main/java/org/jWebSocket/client/WebSocketClient.java
+++ jWebSocketJavaClient/src/main/java/org/jWebSocket/client/WebSocketClient.java
@@ -1,8 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+//	---------------------------------------------------------------------------
+//	jWebSocket - Java WebSocket Client API
+//	Copyright (c) 2010 jWebSocket.org, Alexander Schulze, Innotrade GmbH
+//	---------------------------------------------------------------------------
+//	This program is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU General Public License as published by the
+//	Free Software Foundation; either version 3 of the License, or (at your
+//	option) any later version.
+//	This program is distributed in the hope that it will be useful, but WITHOUT
+//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+//	more details.
+//	You should have received a copy of the GNU General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/>.
+//	---------------------------------------------------------------------------
 package org.jWebSocket.client;
 
 import org.jwebsocket.api.WebSocketPaket;
@@ -36,16 +46,16 @@ public interface WebSocketClient {
 	/**
 	 * Allows the application to send a data packet to the server.
 	 *
-	 * @param aDataPacket
+	 * @param aRawPacket
 	 */
-	void sendPacket(WebSocketPaket aDataPacket);
+	void sendPacket(WebSocketPaket aRawPacket);
 	
 	/**
 	 * Notifies the application that a data packet has been received from
 	 * the server.
 	 *
-	 * @param aDataPacket
+	 * @param aRawPacket
 	 */
-	void processPacket(WebSocketPaket aDataPacket);
+	void processPacket(WebSocketPaket aRawPacket);
 
 }
