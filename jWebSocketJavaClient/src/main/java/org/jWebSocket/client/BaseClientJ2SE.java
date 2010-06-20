147e01dd0a92ec4cde3e013e080d5ff98c30030b
mailtopuran@gmail.com <mailtopuran@gmail.com@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-06-20 05:28:13 +0000

    renamed the package name

--- jWebSocketJavaClient/src/main/java/org/jWebSocket/client/BaseClientJ2SE.java
+++ jWebSocketJavaClient/src/main/java/org/jWebSocket/client/BaseClientJ2SE.java
@@ -13,7 +13,7 @@
 //	You should have received a copy of the GNU General Public License along
 //	with this program; if not, see <http://www.gnu.org/licenses/>.
 //	---------------------------------------------------------------------------
-package org.jWebSocket.client;
+package org.jwebsocket.client;
 
 import org.jwebsocket.kit.WebSocketEvent;
 import org.jwebsocket.api.WebSocketListener;
--- jWebSocketJavaClient/src/main/java/org/jWebSocket/client/CGIClient.java
+++ jWebSocketJavaClient/src/main/java/org/jWebSocket/client/CGIClient.java
@@ -13,7 +13,7 @@
 //	You should have received a copy of the GNU General Public License along
 //	with this program; if not, see <http://www.gnu.org/licenses/>.
 //	---------------------------------------------------------------------------
-package org.jWebSocket.client;
+package org.jwebsocket.client;
 
 import java.io.IOException;
 import java.io.InputStream;
