45230749b5dde53874b2bfce73d0ac445405ab8b
mailtopuran@gmail.com <mailtopuran@gmail.com@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-06-20 04:48:28 +0000

    fixed imports

--- jWebSocketNettyEngine/src/main/java/org/jwebsocket/netty/engines/NettyEnginePipeLineFactory.java
+++ jWebSocketNettyEngine/src/main/java/org/jwebsocket/netty/engines/NettyEnginePipeLineFactory.java
@@ -14,8 +14,11 @@
 //	---------------------------------------------------------------------------
 package org.jwebsocket.netty.engines;
 
-import org.jboss.netty.bootstrap.ServerBootstrap;
-import org.jboss.netty.channel.*;
+import org.jboss.netty.channel.ChannelConfig;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.ServerChannel;
 import org.jboss.netty.handler.codec.http.HttpChunkAggregator;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
 import org.jwebsocket.netty.http.HttpRequestDecoder;
