a9b2d16aefa8eff671e99d5c3d391b7fa2e33b71
mailtopuran@gmail.com <mailtopuran@gmail.com@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-06-28 04:36:10 +0000

    jwebsocket client

--- jWebSocketJavaClient/src/main/java/org/jwebsocket/client/nio/JWebSocketClient.java
+++ jWebSocketJavaClient/src/main/java/org/jwebsocket/client/nio/JWebSocketClient.java
@@ -14,20 +14,11 @@
 //  ---------------------------------------------------------------------------
 package org.jwebsocket.client.nio;
 
-import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.jboss.netty.bootstrap.ClientBootstrap;
 import org.jboss.netty.channel.Channel;
-import org.jboss.netty.channel.ChannelFuture;
-import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
-import org.jboss.netty.handler.codec.http.DefaultHttpRequest;
-import org.jboss.netty.handler.codec.http.HttpHeaders;
-import org.jboss.netty.handler.codec.http.HttpMethod;
 import org.jboss.netty.handler.codec.http.HttpRequest;
-import org.jboss.netty.handler.codec.http.HttpVersion;
 import org.jwebsocket.client.BaseClient;
 import org.jwebsocket.kit.WebSocketException;
 
@@ -38,9 +29,12 @@ import org.jwebsocket.kit.WebSocketException;
  * @version $Id$
  */
 public class JWebSocketClient extends BaseClient {
-    
+
     private volatile boolean isConnected = false;
     private Channel channel = null;
+
+    private JWebSocketConnectionManager connectionManager = new JWebSocketConnectionManager();
+    ExecutorService service = Executors.newCachedThreadPool();
 
     @Override
     public void close() throws WebSocketException {
@@ -49,59 +43,29 @@ public class JWebSocketClient extends BaseClient {
 
     @Override
     public boolean isConnected() {
-        return isConnected;
+        return (isConnected && (channel != null && channel.isConnected()));
     }
 
     @Override
     public void open(String aURL) throws WebSocketException {
-        URI uri = null;
-        try {
-            uri = new URI(aURL);
-        } catch (URISyntaxException e) {
-        }
-        String scheme = uri.getScheme() == null ? "ws" : uri.getScheme();
-        String host = uri.getHost() == null ? "localhost" : uri.getHost();
-        int port = uri.getPort();
-        if (port == -1) {
-            if (scheme.equalsIgnoreCase("ws")) {
-                port = 80;
-            } else if (scheme.equalsIgnoreCase("wss")) {
-                port = 443;
-            }
-        }
-
-        boolean secure = scheme.equalsIgnoreCase("wss");
-
-        // Configure the client.
-        ClientBootstrap bootstrap = new ClientBootstrap(new NioClientSocketChannelFactory(Executors.newCachedThreadPool(), 
-                Executors.newCachedThreadPool()));
-
-        // Set up the event pipeline factory.
-        bootstrap.setPipelineFactory(new JWebSocketChannelPipelineFactory(secure, this));
-
-        // Start the connection attempt.
-        ChannelFuture future = bootstrap.connect(new InetSocketAddress(host, port));
-
-        // Wait until the connection attempt succeeds or fails.
-        channel = future.awaitUninterruptibly().getChannel();
-        if (!future.isSuccess()) {
-            future.getCause().printStackTrace();
-            bootstrap.releaseExternalResources();
+        //open the channel
+        channel = connectionManager.openWebSocektConnection(aURL, this);
+        if (channel == null) {
+            isConnected = false;
             return;
-        } 
-        // Prepare the HTTP request.
-        HttpRequest request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, uri.toASCIIString());
-        request.setHeader(HttpHeaders.Names.HOST, host);
-        request.setHeader(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE);
-        request.setHeader(HttpHeaders.Names.ACCEPT_ENCODING, HttpHeaders.Values.GZIP);
-        // Send the HTTP request.
+        }
+        //send the client handshake request
+        HttpRequest request = connectionManager.getWebSocketHandShakeRequest();
         channel.write(request);
 
-        // Wait for the server to close the connection.
-        channel.getCloseFuture().awaitUninterruptibly();
-
-        // Shut down executor threads to exit.
-        bootstrap.releaseExternalResources();
+        // wait for the channel to close normally or abnormally
+        // and clean up the resources.
+        service.execute(new Runnable() {
+            @Override
+            public void run() {
+                connectionManager.closeOperation(channel);
+            }
+        });
     }
 
     @Override
@@ -120,5 +84,4 @@ public class JWebSocketClient extends BaseClient {
     @Override
     public void send(byte[] aData) throws WebSocketException {
     }
-
 }
