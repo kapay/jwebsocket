f0c5cd4d2c844fb9af0383fb47d21547317767e5
mailtopuran@gmail.com <mailtopuran@gmail.com@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-07-18 18:07:57 +0000

    added comments to give the credit to original author

--- jWebSocketJavaSEClient/src/main/java/org/jwebsocket/client/java/WebSocketHandshake.java
+++ jWebSocketJavaSEClient/src/main/java/org/jwebsocket/client/java/WebSocketHandshake.java
@@ -1,3 +1,18 @@
+//  ---------------------------------------------------------------------------
+//  jWebSocket - WebSocket Client Interface
+//  Copyright (c) 2010 jWebSocket.org, Alexander Schulze, Innotrade GmbH
+//  ---------------------------------------------------------------------------
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU Lesser General Public License as published by the
+//  Free Software Foundation; either version 3 of the License, or (at your
+//  option) any later version.
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//  more details.
+//  You should have received a copy of the GNU Lesser General Public License along
+//  with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//  ---------------------------------------------------------------------------
 package org.jwebsocket.client.java;
 
 import java.net.URI;
@@ -8,7 +23,11 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 import org.jwebsocket.kit.WebSocketException;
-
+/**
+ * Based on the implementation from http://weberknecht.googlecode.com
+ * @author Roderick Baier
+ * @version $Id:$
+ */
 public class WebSocketHandshake {
 	
 	private String key1 = null;
