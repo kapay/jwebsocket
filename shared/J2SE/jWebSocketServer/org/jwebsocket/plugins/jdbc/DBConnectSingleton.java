a94d5436ef5215a4254b9b4dde17169b3db01ed4
fivefeetfurther <fivefeetfurther@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-08-18 10:40:38 +0000

    Releasing v0.10.0818 beta (part 2)

--- shared/J2SE/jWebSocketServer/org/jwebsocket/plugins/jdbc/DBConnectSingleton.java
+++ shared/J2SE/jWebSocketServer/org/jwebsocket/plugins/jdbc/DBConnectSingleton.java
@@ -3,26 +3,26 @@
 //	Copyright (c) 2003-2009 Alexander Schulze, Innotrade GmbH
 //	---------------------------------------------------------------------------
 //	This program is free software; you can redistribute it and/or modify it
-//	under the terms of the GNU General Public License as published by the
+//	under the terms of the GNU Lesser General Public License as published by the
 //	Free Software Foundation; either version 3 of the License, or (at your
 //	option) any later version.
 //	This program is distributed in the hope that it will be useful, but WITHOUT
 //	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
 //	more details.
-//	You should have received a copy of the GNU General Public License along
-//	with this program; if not, see <http://www.gnu.org/licenses/>.
+//	You should have received a copy of the GNU Lesser General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
 //	---------------------------------------------------------------------------
 //	Dieses Programm ist freie Software. Sie können es unter den Bedingungen der
-//	GNU General Public License, wie von der Free Software Foundation
+//	GNU Lesser General Public License, wie von der Free Software Foundation
 //	veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß Version 3
 //	der Lizenz oder (nach Ihrer Option) jeder späteren Version.
 //	Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen
 //	von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die
 //	implizite Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN
-//	BESTIMMTEN ZWECK. Details finden Sie in der GNU General Public License.
-//	Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem
-//	Programm erhalten haben. Falls nicht, siehe <http://www.gnu.org/licenses/>.
+//	BESTIMMTEN ZWECK. Details finden Sie in der GNU Lesser General Public License.
+//	Sie sollten ein Exemplar der GNU Lesser General Public License zusammen mit diesem
+//	Programm erhalten haben. Falls nicht, siehe <http://www.gnu.org/licenses/lgpl.html>.
 //	---------------------------------------------------------------------------
 package org.jwebsocket.plugins.jdbc;
 
