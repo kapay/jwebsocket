f93bac645c824081fca180d3b94bf1be69a13b8e
mailtopuran@gmail.com <mailtopuran@gmail.com@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-08-19 07:56:23 +0000

    before stopping the connector and closing the channel check if it is already closed

--- shared/J2SE/jWebSocketServer/org/jwebsocket/netty/connectors/NettyConnector.java
+++ shared/J2SE/jWebSocketServer/org/jwebsocket/netty/connectors/NettyConnector.java
@@ -74,16 +74,16 @@ public class NettyConnector extends BaseConnector {
 	@Override
 	public void stopConnector(CloseReason aCloseReason) {
 		if (log.isDebugEnabled()) {
-			log.debug("Stopping Netty connector (" + aCloseReason.name()
-					+ ")...");
+			log.debug("Stopping Netty connector (" + aCloseReason.name()+ ")...");
 		}
 
 		getEngine().connectorStopped(this, aCloseReason);
-		handler.getChannelHandlerContext().getChannel().close();
-
+		
+		if (handler.getChannelHandlerContext().getChannel().isConnected() && getEngine().isAlive()) {
+			handler.getChannelHandlerContext().getChannel().close();
+		}
 		if (log.isInfoEnabled()) {
-			log.info("Stopped Netty connector (" + aCloseReason.name()
-					+ ") on port "+ getRemotePort());
+			log.info("Stopped Netty connector (" + aCloseReason.name()+ ") on port "+ getRemotePort());
 		}
 	}
 
