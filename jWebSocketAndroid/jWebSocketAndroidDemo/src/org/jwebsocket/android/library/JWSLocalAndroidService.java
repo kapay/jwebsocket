b55635e37b021278c77f0febb8fe450c4575667a
mailtopuran <mailtopuran@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-08-29 20:45:03 +0000

    added sample as a documentation to bind the JWSAndroidLocalService

--- jWebSocketAndroid/jWebSocketAndroidDemo/src/org/jwebsocket/android/library/JWSLocalAndroidService.java
+++ jWebSocketAndroid/jWebSocketAndroidDemo/src/org/jwebsocket/android/library/JWSLocalAndroidService.java
@@ -49,6 +49,51 @@ import android.widget.Toast;
  * the same service which handles multiple connections via multiple 
  * processes.
  * 
+ * The sample code to bid to this service using onCreate() method of Activity:
+ * 
+ * <pre>
+ * private JWSLocalAndroidService mBoundService = null;
+ * 
+ * private ServiceConnection mConnection = new ServiceConnection() {
+ *   public void onServiceConnected(ComponentName className, IBinder service) {
+ *      // This is called when the connection with the service has been
+ *      // established, giving us the service object we can use to
+ *      // interact with the service.  Because we have bound to a explicit
+ *      // service that we know is running in our own process, we can
+ *      // cast its IBinder to a concrete class and directly access it.
+ *      mBoundService = ((JWSLocalAndroidService.LocalBinder)service).getService();
+ *       
+ *   public void onServiceDisconnected(ComponentName className) {
+ *      // This is called when the connection with the service has been
+ *      // unexpectedly disconnected -- that is, its process crashed.
+ *      // Because it is running in our same process, we should never
+ *      // see this happen.
+ *      mBoundService = null;
+ *   };
+ *      
+ *   void doBindService() {
+ *      // Establish a connection with the service.  We use an explicit
+ *      // class name because we want a specific service implementation that
+ *      // we know will be running in our own process (and thus won't be
+ *      // supporting component replacement by other applications).
+ *      bindService(new Intent(Binding.this, JWSLocalAndroidService.class), mConnection, Context.BIND_AUTO_CREATE);
+ *           mIsBound = true;
+ *   }
+ *   
+ *   void doUnbindService() {
+ *      if (mIsBound) {
+ *         // Detach our existing connection.
+ *         unbindService(mConnection);
+ *         mIsBound = false;
+ *       }
+ *    }
+ *    
+ *    @Override
+ *    protected void onCreate(Bundle savedInstanceState) {
+ *       super.onCreate(savedInstanceState);
+ *       doUnbindService();
+ *    }
+ * </pre>
  * @author aschulze 
  * @author <a href="http://www.purans.net/">Puran Singh</a>
  */
