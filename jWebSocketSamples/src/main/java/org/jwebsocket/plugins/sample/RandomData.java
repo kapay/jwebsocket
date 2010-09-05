25514749bc46f86069843fd78f52b1fa3451559a
mailtopuran@gmail.com <mailtopuran@gmail.com@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-09-05 07:46:12 +0000

    trying to improve the custom plugin and filter injection at compile time

---
+++ jWebSocketSamples/src/main/java/org/jwebsocket/plugins/sample/RandomData.java
@@ -0,0 +1,9 @@
+package org.jwebsocket.plugins.sample;
+
+public class RandomData {
+  private String randomText = "this is random text to display by plugin";
+  
+  public String getRandomText() {
+    return randomText;
+  }
+}
