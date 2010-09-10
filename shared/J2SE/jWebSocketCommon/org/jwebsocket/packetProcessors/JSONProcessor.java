60070413585f88ca1dba6063b32791c75cd5ac94
mailtopuran <mailtopuran@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-09-10 16:58:57 +0000

    i hate commented out code

--- shared/J2SE/jWebSocketCommon/org/jwebsocket/packetProcessors/JSONProcessor.java
+++ shared/J2SE/jWebSocketCommon/org/jwebsocket/packetProcessors/JSONProcessor.java
@@ -36,198 +36,155 @@ import org.jwebsocket.token.Token;
  * converts JSON formatted data packets into tokens and vice versa.
  * @author Alexander Schulze, Roderick Baier (improvements regarding JSON array).
  */
+@SuppressWarnings("rawtypes")
 public class JSONProcessor {
-	
-	// TODO: Logging cannot be used in common module because not supported on all clients
-	// private static Logger log = Logging.getLogger(JSONProcessor.class);
-	/**
-	 * converts a JSON formatted data packet into a token.
-	 * @param aDataPacket
-	 * @return
-	 */
-/*
-	public static Token packetToToken(WebSocketPacket aDataPacket) {
-		Token lToken;
-		try {
-			String lStr = aDataPacket.getString("UTF-8");
-			JSONTokener lJSONTokener = new JSONTokener(lStr);
-			JSONObject lJO = new JSONObject(lJSONTokener);
-			lToken = new Token(lJO);
-		} catch (Exception ex) {
-			// TODO: handle exception
-			// log.error(ex.getClass().getSimpleName() + ": " + ex.getMessage());
-			lToken = null;
-		}
-		return lToken;
-	}
-*/
-/*
-	public static WebSocketPacket tokenToPacket(Token aToken) {
-		WebSocketPacket lPacket = null;
-		try {
-			JSONObject lJSON = aToken.getJSONObject();
-			String lData = lJSON.toString();
-			lPacket = new RawPacket(lData, "UTF-8");
-		} catch (Exception ex) {
-			// TODO: process exception
-			// log.error(ex.getClass().getSimpleName() + ": " + ex.getMessage());
-		}
-		return lPacket;
-	}
-*/
-	/**
-	 * Quentin:
-	 * Recursivly convert a JSONArray to a List of List, Map or Object. 
-	 * (also convert all the JSONObject to Map)
-	 * Example: jsonArrayToList( JSONArray [[1,2,3],{"value": "a"}]) will retour a List{{1,2,3},{"value" => "b"}}
-	 * @param aJsonArray
-	 * @return the list
-	 */
-	
-	@SuppressWarnings("unchecked")
-	public static List jsonArrayToList(JSONArray aJsonArray) {
-		List lList = new FastList();
-		for (int i = 0; i < aJsonArray.length(); i++) {
-			try {
-				Object lSubObject = aJsonArray.get(i) ;
-				if (lSubObject instanceof JSONArray) {
-					lList.add(jsonArrayToList((JSONArray)lSubObject));
-				} else if (lSubObject instanceof JSONObject) {
-					lList.add(jsonObjectToMap((JSONObject)lSubObject));
-				}else {
-					lList.add(aJsonArray.get(i));
-				}
-			} catch (JSONException e) { //Sould never happen: aJsonArray.get(i) will always exists
-			}
-		}
-		return lList ;
-	}
-	/**
-	 * Quentin:
-	 * Recursivly convert a JSONObject to a Map of List, Map or Object. 
-	 * (also convert all the JSONArray to List)
-	 * @param aJsonArray
-	 * @return the list
-	 */	
-	@SuppressWarnings("unchecked")
-	public static Map jsonObjectToMap (JSONObject aJsonObject) {
-		FastMap lFastMap = new FastMap() ;
-		Iterator iterator = aJsonObject.keys();
-		while(iterator.hasNext()) {
-			try {
-				Object aKey = iterator.next();
-				Object aValue = aJsonObject.get(String.valueOf(aKey));
-			    if (aValue instanceof JSONArray) {
-			    	lFastMap.put(aKey, jsonArrayToList((JSONArray)aValue));
-			    } else if  (aValue instanceof JSONObject) {
-			    	lFastMap.put(aKey, jsonObjectToMap((JSONObject)aValue));
-			    } else {
-			    	lFastMap.put(aKey, aValue);
-			    }
-			} catch (JSONException e) { //Sould never happen: aJsonObject.get(String.valueOf(aKey)) will always exists
-			}
-		}
-		return lFastMap;
-	}
-	
-	/**
-	 * converts a JSON formatted data packet into a token.
-	 * @param aDataPacket
-	 * @return
-	 */
-	public static Token packetToToken(WebSocketPacket aDataPacket) {
-		Token lToken = new MapToken();
-		try {
-			String lStr = aDataPacket.getString("UTF-8");
-			JSONTokener lJT = new JSONTokener(lStr);
-			JSONObject lJO = new JSONObject(lJT);
-			for (Iterator lIterator = lJO.keys(); lIterator.hasNext();) {
-				String lKey = (String) lIterator.next();
-				Object lValue = lJO.get(lKey) ;
-				//Quentin:
-				//Maybe we souldn't use the JSON api but directly re-write a "converter" from Json to Token... 
-				//User should always handle List and Map, never JSONObject or JSONArray, since we don't know if xml, csv or json is used as protocol.
-				if( lValue instanceof JSONArray) {
-					lToken.setValidated(lKey, jsonArrayToList((JSONArray) lValue));
-				} else if( lValue instanceof JSONObject) {
-					lToken.setValidated(lKey, jsonObjectToMap((JSONObject) lValue));
-				} else {
-					lToken.setValidated(lKey, lValue);
-				}
-			}
-		} catch (Exception ex) {
-			// // TODO: process exception
-			// log.error(ex.getClass().getSimpleName() + ": " + ex.getMessage());
-		}
-		return lToken;
-	}
 
-	public static WebSocketPacket tokenToPacket(Token token) {
-		WebSocketPacket packet = null;
-		try {
-			JSONObject lJO = tokenToJSON(token);
-			String data = lJO.toString();
-			packet = new RawPacket(data, "UTF-8");
-		} catch (Exception ex) {
-			// TODO: process exception
-			// log.error(ex.getClass().getSimpleName() + ": " + ex.getMessage());
-		}
+    /**
+     * Quentin: Recursivly convert a JSONArray to a List of List, Map or Object.
+     * (also convert all the JSONObject to Map) Example: jsonArrayToList(
+     * JSONArray [[1,2,3],{"value": "a"}]) will retour a List{{1,2,3},{"value"
+     * => "b"}}
+     * 
+     * @param aJsonArray
+     * @return the list
+     */
 
-		return packet;
-	}
+    @SuppressWarnings("unchecked")
+    public static List jsonArrayToList(JSONArray aJsonArray) {
+        List lList = new FastList();
+        for (int i = 0; i < aJsonArray.length(); i++) {
+            try {
+                Object lSubObject = aJsonArray.get(i);
+                if (lSubObject instanceof JSONArray) {
+                    lList.add(jsonArrayToList((JSONArray) lSubObject));
+                } else if (lSubObject instanceof JSONObject) {
+                    lList.add(jsonObjectToMap((JSONObject) lSubObject));
+                } else {
+                    lList.add(aJsonArray.get(i));
+                }
+            } catch (JSONException e) { 
+                // Sould never happen: aJsonArray.get(i)
+                // will always exists
+            }
+        }
+        return lList;
+    }
 
-	private static JSONObject tokenToJSON(Token aToken) throws JSONException {
-		JSONObject lJSO = new JSONObject();
+    /**
+     * Quentin: Recursivly convert a JSONObject to a Map of List, Map or Object.
+     * (also convert all the JSONArray to List)
+     * 
+     * @param aJsonArray
+     * @return the list
+     */
+    @SuppressWarnings("unchecked")
+    public static Map jsonObjectToMap(JSONObject aJsonObject) {
+        FastMap lFastMap = new FastMap();
+        Iterator iterator = aJsonObject.keys();
+        while (iterator.hasNext()) {
+            try {
+                Object aKey = iterator.next();
+                Object aValue = aJsonObject.get(String.valueOf(aKey));
+                if (aValue instanceof JSONArray) {
+                    lFastMap.put(aKey, jsonArrayToList((JSONArray) aValue));
+                } else if (aValue instanceof JSONObject) {
+                    lFastMap.put(aKey, jsonObjectToMap((JSONObject) aValue));
+                } else {
+                    lFastMap.put(aKey, aValue);
+                }
+            } catch (JSONException e) { 
+                // Sould never happen:
+                // aJsonObject.get(String.valueOf(aKey))
+                // will always exists
+            }
+        }
+        return lFastMap;
+    }
 
-		Iterator<String> iterator = aToken.getKeyIterator();
-		while (iterator.hasNext()) {
-			String key = iterator.next();
-			Object value = aToken.getObject(key);
-			if (value instanceof Collection) {
-				JSONArray lArray = new JSONArray();
-				for (Object item : (Collection) value) {
-					if (item instanceof Token) {
-						JSONObject object = tokenToJSON((Token) item);
-						lArray.put(object);
-					} else {
-						lArray.put(item);
-					}
-				}
-				lJSO.put(key, lArray);
-			} else if (value instanceof Token) {
-				lJSO.put(key, tokenToJSON((Token) value));
-			} else if (value instanceof Object[]) {
-				JSONArray lArray = new JSONArray();
-				Object[] lObjs = (Object[]) value;
-				for (int lIdx = 0; lIdx < lObjs.length; lIdx++) {
-					Object lObj = lObjs[lIdx];
-					lArray.put(lObj);
-				}
-				lJSO.put(key, lArray);
-			} else {
-				lJSO.put(key, value);
-			}
-		}
+    /**
+     * converts a JSON formatted data packet into a token.
+     * 
+     * @param aDataPacket
+     * @return
+     */
+    public static Token packetToToken(WebSocketPacket aDataPacket) {
+        Token lToken = new MapToken();
+        try {
+            String lStr = aDataPacket.getString("UTF-8");
+            JSONTokener lJT = new JSONTokener(lStr);
+            JSONObject lJO = new JSONObject(lJT);
+            for (Iterator lIterator = lJO.keys(); lIterator.hasNext();) {
+                String lKey = (String) lIterator.next();
+                Object lValue = lJO.get(lKey);
+                // Quentin:
+                // Maybe we souldn't use the JSON api but directly re-write a
+                // "converter" from Json to Token...
+                // User should always handle List and Map, never JSONObject or
+                // JSONArray, since we don't know if xml, csv or json is used as
+                // protocol.
+                if (lValue instanceof JSONArray) {
+                    lToken.setValidated(lKey, jsonArrayToList((JSONArray) lValue));
+                } else if (lValue instanceof JSONObject) {
+                    lToken.setValidated(lKey, jsonObjectToMap((JSONObject) lValue));
+                } else {
+                    lToken.setValidated(lKey, lValue);
+                }
+            }
+        } catch (Exception ex) {
+            // // TODO: process exception
+            // log.error(ex.getClass().getSimpleName() + ": " +
+            // ex.getMessage());
+        }
+        return lToken;
+    }
+    
+    public static WebSocketPacket tokenToPacket(Token token) {
+        WebSocketPacket packet = null;
+        try {
+            JSONObject lJO = tokenToJSON(token);
+            String data = lJO.toString();
+            packet = new RawPacket(data, "UTF-8");
+        } catch (Exception ex) {
+            // TODO: process exception
+            // log.error(ex.getClass().getSimpleName() + ": " +
+            // ex.getMessage());
+        }
 
-		return lJSO;
-	}
+        return packet;
+    }
 
-	/**
-	 * Creates a token based on a JSON object
-	 */
-/*
-	public static Token JSON2Token(JSONObject aJSON) {
-		Token lToken = new Token();
-		Iterator lIterator = aJSON.keys();
-		while( lIterator.hasNext() ) {
-			String lKey = (String)lIterator.next();
-			try {
-				lToken.put(lKey, aJSON.get(lKey));
-			} catch (JSONException ex) {
-				// TODO: handle exception!
-			}
-		}
-		return lToken;
-	}
-*/
+    private static JSONObject tokenToJSON(Token aToken) throws JSONException {
+        JSONObject lJSO = new JSONObject();
+
+        Iterator<String> iterator = aToken.getKeyIterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            Object value = aToken.getObject(key);
+            if (value instanceof Collection) {
+                JSONArray lArray = new JSONArray();
+                for (Object item : (Collection) value) {
+                    if (item instanceof Token) {
+                        JSONObject object = tokenToJSON((Token) item);
+                        lArray.put(object);
+                    } else {
+                        lArray.put(item);
+                    }
+                }
+                lJSO.put(key, lArray);
+            } else if (value instanceof Token) {
+                lJSO.put(key, tokenToJSON((Token) value));
+            } else if (value instanceof Object[]) {
+                JSONArray lArray = new JSONArray();
+                Object[] lObjs = (Object[]) value;
+                for (int lIdx = 0; lIdx < lObjs.length; lIdx++) {
+                    Object lObj = lObjs[lIdx];
+                    lArray.put(lObj);
+                }
+                lJSO.put(key, lArray);
+            } else {
+                lJSO.put(key, value);
+            }
+        }
+        return lJSO;
+    }
 }
