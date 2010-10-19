2ce28108a0ee1e9ecb8af879eaaa7092221cc509
mailtopuran@gmail.com <mailtopuran@gmail.com@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-10-19 05:05:06 +0000

    channel listener interface for listening to channel events

---
+++ shared/J2SE/jWebSocketServer/org/jwebsocket/plugins/channels/ChannelListener.java
@@ -0,0 +1,28 @@
+//  ---------------------------------------------------------------------------
+//  jWebSocket - Copyright (c) 2010 jwebsocket.org
+//  ---------------------------------------------------------------------------
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU Lesser General Public License as published by the
+//  Free Software Foundation; either version 3 of the License, or (at your
+//  option) any later version.
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//  more details.
+//  You should have received a copy of the GNU Lesser General Public License along
+//  with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//  ---------------------------------------------------------------------------
+package org.jwebsocket.plugins.channels;
+
+public interface ChannelListener {
+    
+    void channelStarted(Channel channel);
+    
+    void channelStopped(Channel channel);
+    
+    void channelSuspended(Channel channel);
+    
+    void subscribed(Channel channel, Subscriber subscriber);
+    
+    void unsubscribed(Channel channel, Subscriber subscriber);
+}
