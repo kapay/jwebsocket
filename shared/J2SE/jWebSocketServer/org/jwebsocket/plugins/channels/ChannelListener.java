9fddd08fb019a9ab38477ee26f7b707e4fde30f3
mailtopuran@gmail.com <mailtopuran@gmail.com@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-10-19 05:15:32 +0000

    added javadoc for listener

--- shared/J2SE/jWebSocketServer/org/jwebsocket/plugins/channels/ChannelListener.java
+++ shared/J2SE/jWebSocketServer/org/jwebsocket/plugins/channels/ChannelListener.java
@@ -13,16 +13,38 @@
 //  with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
 //  ---------------------------------------------------------------------------
 package org.jwebsocket.plugins.channels;
-
+/**
+ * Listener interface for the <tt>Channel</tt>. 
+ * @author puran
+ * @version $Id$
+ */
 public interface ChannelListener {
-    
+    /**
+     * Called when the channel is started.
+     * @param channel the started channel
+     */
     void channelStarted(Channel channel);
-    
+    /**
+     * called when the channel is stopped for receiving/sending any data
+     * @param channel the stopped channel object
+     */
     void channelStopped(Channel channel);
-    
+    /**
+     * called when the channel is suspended that means the channel suspended
+     * can no longer be used to send and receive data unless it is started again 
+     * @param channel the suspended channel
+     */
     void channelSuspended(Channel channel);
-    
+    /**
+     * called when a new subscriber subscribes the channel
+     * @param channel the channel 
+     * @param subscriber the subscriber
+     */
     void subscribed(Channel channel, Subscriber subscriber);
-    
+    /**
+     * called when someone unsuscribes the channel
+     * @param channel the channel 
+     * @param subscriber the subscriber who unsuscribed
+     */
     void unsubscribed(Channel channel, Subscriber subscriber);
 }
