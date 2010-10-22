3c289a5bc37fce2907b923d23235c7c9d517afe4
fivefeetfurther <fivefeetfurther@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-10-22 13:30:33 +0000

    Cleaned up jWebSocket.js library - now RPC/RRPC and Streaming-PlugIn located in separate files. Adjusted demos accordingly.

--- jWebSocketClient/web/res/js/jWebSocket.js
+++ jWebSocketClient/web/res/js/jWebSocket.js
@@ -310,6 +310,7 @@ if( !jws.browserSupportsNativeJSON ) {
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(input){var output="";var chr1,chr2,chr3,enc1,enc2,enc3,enc4;var i=0;input=Base64._utf8_encode(input);while(i<input.length){chr1=input.charCodeAt(i++);chr2=input.charCodeAt(i++);chr3=input.charCodeAt(i++);enc1=chr1>>2;enc2=((chr1&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64;}else if(isNaN(chr3)){enc4=64;}output=output+this._keyStr.charAt(enc1)+this._keyStr.charAt(enc2)+this._keyStr.charAt(enc3)+this._keyStr.charAt(enc4);}return output;},decode:function(input){var output="";var chr1,chr2,chr3;var enc1,enc2,enc3,enc4;var i=0;input=input.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(i<input.length){enc1=this._keyStr.indexOf(input.charAt(i++));enc2=this._keyStr.indexOf(input.charAt(i++));enc3=this._keyStr.indexOf(input.charAt(i++));enc4=this._keyStr.indexOf(input.charAt(i++));chr1=(enc1<<2)|(enc2>>4);chr2=((enc2&15)<<4)|(enc3>>2);chr3=((enc3&3)<<6)|enc4;output=output+String.fromCharCode(chr1);if(enc3!=64){output=output+String.fromCharCode(chr2);}if(enc4!=64){output=output+String.fromCharCode(chr3);}}output=Base64._utf8_decode(output);return output;},_utf8_encode:function(string){string=string.replace(/\r\n/g,"\n");var utftext="";for(var n=0;n<string.length;n++){var c=string.charCodeAt(n);if(c<128){utftext+=String.fromCharCode(c);}else if((c>127)&&(c<2048)){utftext+=String.fromCharCode((c>>6)|192);utftext+=String.fromCharCode((c&63)|128);}else{utftext+=String.fromCharCode((c>>12)|224);utftext+=String.fromCharCode(((c>>6)&63)|128);utftext+=String.fromCharCode((c&63)|128);}}return utftext;},_utf8_decode:function(utftext){var string="";var i=0;var c=c1=c2=0;while(i<utftext.length){c=utftext.charCodeAt(i);if(c<128){string+=String.fromCharCode(c);i++;}else if((c>191)&&(c<224)){c2=utftext.charCodeAt(i+1);string+=String.fromCharCode(((c&31)<<6)|(c2&63));i+=2;}else{c2=utftext.charCodeAt(i+1);c3=utftext.charCodeAt(i+2);string+=String.fromCharCode(((c&15)<<12)|((c2&63)<<6)|(c3&63));i+=3;}}return string;}}
 //	</JasobNoObfs>
 
+
 //	---------------------------------------------------------------------------
 //  jWebSocket - some convenience JavaScript OOP tools
 //	---------------------------------------------------------------------------
@@ -356,6 +357,7 @@ jws.oop.declareClass = function( aNamespace, aClassname, aAncestor, aFields ) {
 		}
 	}
 };
+
 
 // plug-in functionality to allow to add plug-ins into existing classes
 jws.oop.addPlugIn = function( aClass, aPlugIn ) {
@@ -1488,263 +1490,6 @@ jws.oop.addPlugIn( jws.jWebSocketTokenClient, jws.SystemClientPlugIn );
 
 
 //	---------------------------------------------------------------------------
-//  jWebSocket Client Streaming Plug-In
-//	---------------------------------------------------------------------------
-
-//:package:*:jws
-//:class:*:jws.StreamingPlugIn
-//:ancestor:*:-
-//:d:en:Implementation of the [tt]jws.StreamingPlugIn[/tt] class. This _
-//:d:en:plug-in provides the methods to register and unregister at certain _
-//:d:en:stream sn the server.
-jws.StreamingPlugIn = {
-
-	//:const:*:NS:String:org.jwebsocket.plugins.streaming (jws.NS_BASE + ".plugins.streaming")
-	//:d:en:Namespace for the [tt]StreamingPlugIn[/tt] class.
-	// if namespace changed update server plug-in accordingly!
-	NS: jws.NS_BASE + ".plugins.streaming",
-
-	//:m:*:registerStream
-	//:d:en:Registers the client at the given stream on the server. _
-	//:d:en:After this operation the client obtains all messages in this _
-	//:d:en:stream. Basically a client can register at multiple streams.
-	//:d:en:If no stream with the given ID exists on the server an error token _
-	//:d:en:is returned. Depending on the type of the stream it may take more _
-	//:d:en:or less time until you get the first token from the stream.
-	//:a:en::aStream:String:The id of the server side data stream.
-	//:r:*:::void:none
-	// TODO: introduce OnResponse here too to get noticed on error or success.
-	registerStream: function( aStream ) {
-		var lRes = this.createDefaultResult();
-		if( this.isConnected() ) {
-			this.sendToken({
-				ns: jws.StreamingPlugIn.NS,
-				type: "register",
-				stream: aStream
-			});
-		} else {
-			lRes.code = -1;
-			lRes.localeKey = "jws.jsc.res.notConnected";
-			lRes.msg = "Not connected.";
-		}
-		return lRes;
-	},
-
-	//:m:*:unregisterStream
-	//:d:en:Unregisters the client from the given stream on the server.
-	//:a:en::aStream:String:The id of the server side data stream.
-	//:r:*:::void:none
-	// TODO: introduce OnResponse here too to get noticed on error or success.
-	unregisterStream: function( aStream ) {
-		var lRes = this.createDefaultResult();
-		if( this.isConnected() ) {
-			this.sendToken({
-				ns: jws.StreamingPlugIn.NS,
-				type: "unregister",
-				stream: aStream
-			});
-		} else {
-			lRes.code = -1;
-			lRes.localeKey = "jws.jsc.res.notConnected";
-			lRes.msg = "Not connected.";
-		}
-		return lRes;
-	}
-};
-
-// add the StreamingPlugIn PlugIn into the jWebSocketTokenClient class
-jws.oop.addPlugIn( jws.jWebSocketTokenClient, jws.StreamingPlugIn );
-
-
-//:package:*:jws
-//:class:*:jws.RPCClientPlugIn
-//:ancestor:*:-
-//:d:en:Implementation of the [tt]jws.RPCClientPlugIn[/tt] class.
-jws.RPCClientPlugIn = {
-
-	// granted rrpc's
-	grantedProcs: [],
-	
-	// granted rrpc's
-	spawnThreadDefault: false,
-
-	//:const:*:NS:String:org.jwebsocket.plugins.rpc (jws.NS_BASE + ".plugins.rpc")
-	//:d:en:Namespace for the [tt]RPCClientPlugIn[/tt] class.
-	// if namespace changed update server plug-in accordingly!
-	NS: jws.NS_BASE + ".plugins.rpc",
-	
-	//:m:*:setSpawnThreadDefault
-	//:d:en:set the default value of the spawnThread option 
-	//:a:en::aDefault:Boolean.
-	//:r:*:::void:none
-	setSpawnThreadDefault: function (aDefault) {
-		this.spawnThreadDefault = aDefault;
-	},
-	
-	//:m:*:addGrantedProcedure
-	//:d:en:grant the access to a rrpc procedure
-	//:a:en::aProcedure:String procedure name (including name space).
-	//:r:*:::void:none
-	addGrantedProcedure: function (aProcedure) {
-		jws.RPCClientPlugIn.grantedProcs[ jws.RPCClientPlugIn.grantedProcs.length ] = aProcedure;
-	},
-	
-	//:m:*:removeGrantedProcedure
-	//:d:en:remove the access to a rrpc procedure
-	//:a:en::aProcedure:String procedure name (including name space).
-	//:r:*:::void:none
-	removeGrantedProcedure: function (aProcedure) {
-		var lIdx = jws.RPCClientPlugIn.grantedProcs.indexOf( aProcedure );
-		if( lIdx >= 0 ) {
-			jws.RPCClientPlugIn.grantedProcs.splice( lIdx, 1 );
-		}
-	},
-	
-	//:m:*:processToken
-	//:d:en:Processes an incoming token from the server or a remote client. _
-	//:d:en:Here the token is checked for type [tt]rrpc[/tt]. If such is _
-	//:d:en:detected it gets processed by the [tt]onRRPC[/tt] method of this class.
-	//:a:en::aToken:Object:Token received from the server or a remote client.
-	//:r:*:::void:none
-	processToken: function( aToken ) {
-		// console.log( "jws.RPCClientPlugIn: Processing token " + aToken.ns + "/" + aToken.type + "..." );
-		if( aToken.ns == jws.RPCClientPlugIn.NS ) {
-			if( aToken.type == "rrpc" ) {
-				this.onRRPC( aToken );
-			}
-		}
-	},
-
-	//:m:*:rpc
-	//:d:en:Runs a remote procedure call (RPC) on the jWebSocket server. _
-	//:d:en:The security mechanisms on the server require the call to be _
-	//:d:en:granted, otherwise it gets rejected.
-	//:a:en::aClass:String:Class of the method that is supposed to be called.
-	//:a:en::aMthd:String:Name of the method that is supposed to be called.
-	//:a:en::aArgs:Array:Arguments for method that is supposed to be called. Should always be an array, but also works with simple values. Caution with a simple array as parameter (args mus be: [[1,2..]]).
-	//:a:en::aOptions:Object:Optional arguments. For details please refer to the [tt]sendToken[/tt] method.
-	//:r:*:::void:none
-	rpc: function( aClass, aMthd, aArgs, aOptions) {
-		if (aArgs != null && !(aArgs instanceof Array)) {
-			aArgs = [aArgs];
-		}
-		aOptions = this.setDefaultOption (aOptions) ;
-		var lRes = this.createDefaultResult();
-		if( this.isConnected() ) {
-			this.sendToken({
-				ns: jws.RPCClientPlugIn.NS,
-				type: "rpc",
-				classname: aClass,
-				method: aMthd,
-				args: aArgs
-				},
-				aOptions
-			);
-		} else {
-			lRes.code = -1;
-			lRes.localeKey = "jws.jsc.res.notConnected";
-			lRes.msg = "Not connected.";
-		}
-		return lRes;
-	},
-
-	setDefaultOption: function( aOptions ) {
-		if (aOptions === undefined) {
-			aOptions = {} ;
-		}
-		if (aOptions.spawnThread === undefined) {
-			aOptions.spawnThread = this.spawnThreadDefault;
-		}
-		return aOptions ;
-	},
-
-	//:m:*:rrpc
-	//:d:en:Runs a reverse remote procedure call (RRPC) on another client.
-	//:a:en::aTarget:String:Id of the target remote client.
-	//:a:en::aClass:String:Class of the method that is supposed to be called.
-	//:a:en::aMthd:String:Name of the method that is supposed to be called.
-	//:a:en::aArgs:Array:Arguments for method that is supposed to be called.
-	//:a:en::aOptions:Object:Optional arguments. For details please refer to the [tt]sendToken[/tt] method.
-	//:r:*:::void:none
-	rrpc: function( aTarget, aClass, aMthd, aArgs, aOptions ) {
-		if (aArgs != null && !(aArgs instanceof Array)) {
-			aArgs = [aArgs];
-		}
-		aOptions = this.setDefaultOption (aOptions) ;
-		var lRes = this.createDefaultResult();
-		if( this.isConnected() ) {
-			this.sendToken({
-				ns: jws.RPCClientPlugIn.NS,
-				type: "rrpc",
-				targetId: aTarget,
-				classname: aClass,
-				method: aMthd,
-				args: aArgs
-			},
-			aOptions
-			);
-		} else {
-			lRes.code = -1;
-			lRes.localeKey = "jws.jsc.res.notConnected";
-			lRes.msg = "Not connected.";
-		}
-		return lRes;
-	},
-
-	//:m:*:onRRPC
-	//:d:en:Processes a remote procedure call from another client. _
-	//:d:en:This method is called internally only and should not be invoked _
-	//:d:en:by the application.
-	//:a:en::aToken:Object:Token that contains the rrpc arguments from the source client.
-	//:r:*:::void:none
-	onRRPC: function( aToken ) {
-		var lClassname = aToken.classname;
-		var lMethod = aToken.method;
-		var lArgs = aToken.args;
-		var lPath = lClassname + "." + lMethod;
-		// check if the call is granted on this client
-		if( jws.RPCClientPlugIn.grantedProcs.indexOf( lPath ) >= 0 ) {
-			var lFunctionSplit = lClassname.split( '.' );
-			var lFunctionSplitSize = lFunctionSplit.length;
-			var lTheFunction = window[ lFunctionSplit[ 0 ] ] ;
-			for( var j = 1; j < lFunctionSplitSize; j++ ) {
-				lTheFunction = lTheFunction[ lFunctionSplit[ j ] ];
-			}
-			var lRes;
-			try {
-				lRes = lTheFunction[ lMethod ].apply( null, lArgs);
-			} catch (ex) {
-				//TODO: send back the error under a better format
-				lRes = ex 
-					+ "\nProbably a typo error (method called="
-					+ lMethod
-					+ ") or wrong number of arguments (args: "
-					+ JSON.stringify(lArgs)
-					+ ")";
-			}
-		} else {
-			//TODO: send back the error under a better format
-			lRes = ex 
-			+ "\nAcces not granted to the="
-			+ lMethod;
-		}
-		this.sendToken({
-				// ns: jws.SystemPlugIn.NS,
-				type: "send",
-				targetId: aToken.sourceId,
-				result: lRes,
-				reqType: "rrpc",
-				code: 0
-			},null // aOptions
-		);
-	}	
-}
-
-// add the JWebSocket RPC PlugIn into the BaseClient class
-jws.oop.addPlugIn( jws.jWebSocketTokenClient, jws.RPCClientPlugIn );
-
-
-//	---------------------------------------------------------------------------
 //  jWebSocket JSON client
 //	todo: consider potential security issues with 'eval'
 //	---------------------------------------------------------------------------
@@ -2002,7 +1747,6 @@ jws.oop.declareClass( "jws", "jWebSocketXMLClient", jws.jWebSocketTokenClient, {
 });
 
 /*
-
 (function() {
 	var lObj = {
 		aNumber: 1,
@@ -2044,5 +1788,4 @@ jws.oop.declareClass( "jws", "jWebSocketXMLClient", jws.jWebSocketTokenClient, {
 	var lToken = lXMLClient.streamToToken( lStream );
 	console.log( lStream );
 })();
-
 */
