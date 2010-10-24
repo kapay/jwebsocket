74ee4ad5aabf4d5c63522dcc90b3b4dd924df19d
mailtopuran@gmail.com <mailtopuran@gmail.com@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-10-24 06:01:22 +0000

    publisher store implementation class

---
+++ shared/J2SE/jWebSocketServer/org/jwebsocket/plugins/channels/BasePublisherStore.java
@@ -0,0 +1,35 @@
+package org.jwebsocket.plugins.channels;
+
+public class BasePublisherStore implements PublisherStore {
+
+  @Override
+  public Publisher getPublisher(String id) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public boolean storePublisher(Publisher publisher) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void removePublisher(String id) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void clearPublishers() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public int getPublisherStoreSize() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+}
