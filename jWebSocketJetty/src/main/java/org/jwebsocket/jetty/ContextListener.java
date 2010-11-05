8c42958264d846e98d5240947b98922bdaacfd98
fivefeetfurther <fivefeetfurther@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2010-11-05 08:35:24 +0000

    Jetty Addons

---
+++ jWebSocketJetty/src/main/java/org/jwebsocket/jetty/ContextListener.java
@@ -0,0 +1,47 @@
+//	---------------------------------------------------------------------------
+//	jWebSocket - Context Listener for Jetty Web Applications
+//	Copyright (c) 2010 jWebSocket.org, Alexander Schulze, Innotrade GmbH
+//	---------------------------------------------------------------------------
+//	This program is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Lesser General Public License as published by the
+//	Free Software Foundation; either version 3 of the License, or (at your
+//	option) any later version.
+//	This program is distributed in the hope that it will be useful, but WITHOUT
+//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//	more details.
+//	You should have received a copy of the GNU Lesser General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	---------------------------------------------------------------------------
+package org.jwebsocket.jetty;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import org.jwebsocket.factory.JWebSocketFactory;
+
+/**
+ * Web application lifecycle listener.
+ * @author alexanderschulze
+ */
+public class ContextListener implements ServletContextListener {
+
+	/**
+	 * initializes the web application on startup.
+	 * @param sce
+	 */
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		// start the jWebSocket server sub system
+		JWebSocketFactory.start("");
+	}
+
+	/**
+	 * cleans up the web application on termination.
+	 * @param sce
+	 */
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// stop the jWebSocket server sub system
+		JWebSocketFactory.stop();
+	}
+}
--- jWebSocketJetty/src/main/java/org/jwebsocket/jetty/jWebSocket.java
+++ jWebSocketJetty/src/main/java/org/jwebsocket/jetty/jWebSocket.java
@@ -9,21 +9,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.websocket.WebSocket;
-import org.eclipse.jetty.websocket.WebSocket.Outbound;
 import org.eclipse.jetty.websocket.WebSocketServlet;
-import org.jwebsocket.api.WebSocketConnector;
-import org.jwebsocket.api.WebSocketEngine;
-import org.jwebsocket.api.WebSocketPacket;
-import org.jwebsocket.kit.CloseReason;
-import org.jwebsocket.kit.RawPacket;
 
 /**
  *
  * @author aschulze
  */
 public class jWebSocket extends WebSocketServlet {
-
-	WebSocketEngine mEngine = new JettyEngine(null);
 
 	@Override
 	protected void doGet(HttpServletRequest aRequest, HttpServletResponse aResponse)
@@ -46,51 +38,9 @@ public class jWebSocket extends WebSocketServlet {
 	}
 
 	@Override
-	protected WebSocket doWebSocketConnect(HttpServletRequest request, String protocol) {
+	protected WebSocket doWebSocketConnect(HttpServletRequest aRequest, String aProtocol) {
 		System.out.println("@doWebSocketConnect");
-		return new JWebSocketWrapper();
-	}
-
-	class JWebSocketWrapper implements WebSocket {
-
-		private WebSocketConnector mConnector = null;
-		// Outbound _outbound;
-
-		@Override
-		public void onConnect(Outbound aOutbound) {
-			// _outbound = aOutbound;
-			mConnector = new JettyConnector(mEngine, null);
-			mEngine.addConnector(mConnector);
-			// inherited BaseConnector.startConnector
-			// calls mEngine connector started
-			mConnector.startConnector();
-		}
-
-		@Override
-		public void onMessage(byte frame, byte[] data, int offset, int length) {
-			if (mConnector != null) {
-				WebSocketPacket lDataPacket = new RawPacket(data);
-				mEngine.processPacket(mConnector, null);
-			}
-		}
-
-		@Override
-		public void onMessage(byte frame, String data) {
-			if (mConnector != null) {
-				WebSocketPacket lDataPacket = new RawPacket(data);
-				mEngine.processPacket(mConnector, null);
-			}
-		}
-
-		@Override
-		public void onDisconnect() {
-			if (mConnector != null) {
-				// inherited BaseConnector.stopConnector
-				// calls mEngine connector stopped
-				mConnector.stopConnector(CloseReason.SERVER);
-				mEngine.removeConnector(mConnector);
-			}
-		}
+		return new JettyWrapper(aRequest, aProtocol);
 	}
 
 	/**
