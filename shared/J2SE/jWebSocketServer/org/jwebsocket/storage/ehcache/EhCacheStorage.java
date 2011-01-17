a40c4d39aba74e2e61f0b8202f8afa1db20fe548
fivefeetfurther <fivefeetfurther@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2011-01-17 16:47:34 +0000

    Refactored storage engines to use separate package per engine.

--- shared/J2SE/jWebSocketServer/org/jwebsocket/storage/ehcache/EhCacheStorage.java
+++ shared/J2SE/jWebSocketServer/org/jwebsocket/storage/ehcache/EhCacheStorage.java
@@ -13,7 +13,7 @@
 //  You should have received a copy of the GNU Lesser General Public License along
 //  with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
 //  ---------------------------------------------------------------------------
-package org.jwebsocket.storage;
+package org.jwebsocket.storage.ehcache;
 
 import java.util.Collection;
 import java.util.Map;
