b47353194c3cdf37295aa58ad6d359e12ed9a3b0
fivefeetfurther <fivefeetfurther@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2011-05-16 07:20:32 +0000

    Demo user now select rights only

--- databases/MySQL/grantDemo.sql
+++ databases/MySQL/grantDemo.sql
@@ -1,14 +1,11 @@
 /* This script creates the uses jwsDemo and grants him the "demo" rights 
- * This database user is supposed to be used for external user access for usual work purposes!
+ * This database user is supposed to be used for non-authenticated external user access for demo purposes only!
  */
 
 /* use this database schema */
 use `jwebsocket`;
 
 /*  demo tables */
-grant select,insert,delete,update on `jwebsocket`.`demo_master` to `jwsDemo`@`localhost`;
-grant select,insert,delete,update on `jwebsocket`.`demo_child` to `jwsDemo`@`localhost`;
-grant select,insert,delete,update on `jwebsocket`.`demo_lookup` to `jwsDemo`@`localhost`;
-
-/* necessary access to stored procedures */
-grant execute on procedure `jwebsocket`.`getSequence` to `jwsDemo`@`localhost`;
+grant select on `jwebsocket`.`demo_master` to `jwsDemo`@`localhost`;
+grant select on `jwebsocket`.`demo_child` to `jwsDemo`@`localhost`;
+grant select on `jwebsocket`.`demo_lookup` to `jwsDemo`@`localhost`;
