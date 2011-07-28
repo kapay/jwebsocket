b333b86a26d1a5c136657a008e38191f34ab7495
fivefeetfurther <fivefeetfurther@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2011-07-28 14:34:12 +0000

    Cleanup for 1.0b

--- templates/NetBeans/MyPlugIn/src/org/jwebsocket/plugins/myplugin/MyPlugIn.java
+++ templates/NetBeans/MyPlugIn/src/org/jwebsocket/plugins/myplugin/MyPlugIn.java
@@ -20,7 +20,6 @@ import org.apache.log4j.Logger;
 import org.jwebsocket.api.PluginConfiguration;
 import org.jwebsocket.api.WebSocketConnector;
 import org.jwebsocket.api.WebSocketEngine;
-import org.jwebsocket.config.JWebSocketServerConstants;
 import org.jwebsocket.kit.CloseReason;
 import org.jwebsocket.kit.PlugInResponse;
 import org.jwebsocket.logging.Logging;
