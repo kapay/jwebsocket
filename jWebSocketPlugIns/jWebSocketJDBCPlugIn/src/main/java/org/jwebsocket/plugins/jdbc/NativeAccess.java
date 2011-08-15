f3c10bdbb0dbd88b358f453067949c7dab1f7244
fivefeetfurther <fivefeetfurther@c00d5198-0468-11df-94a2-a5fc3cd617a6>
2011-08-15 08:38:59 +0000

    Added separate plug-ins

---
+++ jWebSocketPlugIns/jWebSocketJDBCPlugIn/src/main/java/org/jwebsocket/plugins/jdbc/NativeAccess.java
@@ -0,0 +1,109 @@
+//	---------------------------------------------------------------------------
+//	jWebSocket - jWebSocket Native SQL Access for JDBC Plug-In
+//  Copyright (c) 2011 Innotrade GmbH, jWebSocket.org
+//	---------------------------------------------------------------------------
+//	This program is free software; you can redistribute it and/or modify it
+//	under the terms of the GNU Lesser General Public License as published by the
+//	Free Software Foundation; either version 3 of the License, or (at your
+//	option) any later version.
+//	This program is distributed in the hope that it will be useful, but WITHOUT
+//	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//	FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+//	more details.
+//	You should have received a copy of the GNU Lesser General Public License along
+//	with this program; if not, see <http://www.gnu.org/licenses/lgpl.html>.
+//	---------------------------------------------------------------------------
+package org.jwebsocket.plugins.jdbc;
+
+import javax.sql.DataSource;
+import org.jwebsocket.token.Token;
+import org.jwebsocket.token.TokenFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+/**
+ *
+ * @author aschulze
+ */
+public class NativeAccess {
+
+	private JdbcTemplate mJDBCTemplate;
+	private String mSelectSequenceSQL = null;
+
+	public void setDataSource(DataSource aDataSource) {
+		mJDBCTemplate = new JdbcTemplate(aDataSource);
+		mJDBCTemplate.setQueryTimeout(10); // seconds
+	}
+
+	public void setSelectSequenceSQL(String aSQL) {
+		mSelectSequenceSQL = aSQL;
+	}
+
+	public String getSelectSequenceSQL() {
+		return mSelectSequenceSQL;
+	}
+
+	public DataSource getDataSource() {
+		return mJDBCTemplate.getDataSource();
+	}
+
+	public Token query(String aSQL, Object[] aArgs) {
+		Token lResToken;
+		SqlRowSet lRowSet;
+		try {
+			if (aArgs != null) {
+				lRowSet = mJDBCTemplate.queryForRowSet(aSQL, aArgs);
+			} else {
+				lRowSet = mJDBCTemplate.queryForRowSet(aSQL);
+			}
+			lResToken = JDBCTools.resultSetToToken(lRowSet);
+		} catch (Exception lEx) {
+			lResToken = TokenFactory.createToken();
+			lResToken.setInteger("code", -1);
+			lResToken.setString("msg",
+					lEx.getClass().getSimpleName() + ": " + lEx.getMessage());
+		}
+		return lResToken;
+	}
+
+	public Token query(String aSQL) {
+		return query(aSQL, null);
+	}
+
+	public Token update(String aSQL, Object[] aArgs) {
+		Token lResToken = TokenFactory.createToken();
+		int lAffectedRows = 0;
+		try {
+			if (aArgs != null) {
+				lAffectedRows = mJDBCTemplate.update(aSQL, aArgs);
+			} else {
+				lAffectedRows = mJDBCTemplate.update(aSQL);
+			}
+			lResToken.setInteger("code", 0);
+		} catch (Exception lEx) {
+			lResToken.setInteger("code", -1);
+			lResToken.setString("msg",
+					lEx.getClass().getSimpleName() + ": " + lEx.getMessage());
+		}
+		lResToken.setInteger("rowsAffected", lAffectedRows);
+
+		return lResToken;
+	}
+
+	public Token update(String aSQL) {
+		return update(aSQL, null);
+	}
+
+	public Token exec(String aSQL) {
+		Token lResToken = TokenFactory.createToken();
+		try {
+			mJDBCTemplate.execute(aSQL);
+			lResToken.setInteger("code", 0);
+		} catch (Exception lEx) {
+			lResToken.setInteger("code", -1);
+			lResToken.setString("msg",
+					lEx.getClass().getSimpleName() + ": " + lEx.getMessage());
+		}
+		return lResToken;
+	}
+}
